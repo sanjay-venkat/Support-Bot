0dddea2d14008b852f26fd9a73e0afe0afff2369
sanjay-venkat <76589651+sanjay-venkat@users.noreply.github.com>
2025-03-11 20:51:15 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,7 @@
 @echo off
 echo Downloading models for faster execution...
-
-:: Download FLAN-T5 Large model
 python -c "from transformers import T5ForConditionalGeneration; T5ForConditionalGeneration.from_pretrained('google/flan-t5-large')"
-
-:: Download FLAN-T5 Large tokenizer
 python -c "from transformers import T5Tokenizer; T5Tokenizer.from_pretrained('google/flan-t5-large')"
-
-:: Download SentenceTransformer model
 python -c "from sentence_transformers import SentenceTransformer; SentenceTransformer('all-MiniLM-L6-v2')"
 
 echo All models downloaded successfully!
