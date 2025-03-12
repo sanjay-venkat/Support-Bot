074a875c5e1251600f1588bb91d6d962969d1647
sanjay-venkat <76589651+sanjay-venkat@users.noreply.github.com>
2025-03-12 10:09:39 +0530

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -5,7 +5,7 @@ if not exist "models" mkdir models
 
 if not exist "models\flan-t5-large" (
     echo Downloading FLAN-T5 model...
-    python -c "from transformers import T5ForConditionalGeneration, T5Tokenizer; model = T5ForConditionalGeneration.from_pretrained('google/flan-t5-large'); tokenizer = T5Tokenizer.from_pretrained('google/flan-t5-large'); model.save_pretrained('models/flan-t5-large');tokenizer.save_pretrained('models/flan-t5-large')"
+    python -c "from transformers import T5ForConditionalGeneration, T5Tokenizer; model = T5ForConditionalGeneration.from_pretrained('google/flan-t5-large'); tokenizer = T5Tokenizer.from_pretrained('google/flan-t5-large'); model.save_pretrained('models/flan-t5-large');tokenizer.save_pretrained('models/flan-t5-large'"
     echo FLAN-T5 model downloaded successfully.
 ) else (
     echo FLAN-T5 model already exists.
@@ -17,6 +17,14 @@ if not exist "models\all-MiniLM-L6-v2" (
     echo Sentence Transformer model downloaded successfully.
 ) else (
     echo Sentence Transformer model already exists.
+)
+
+if not exist "tokenizers/punkt_tab" (
+    echo download punkt....
+    python -c "import nltk; nltk.download('punkt_tab')"
+    echo punkt downloaded successfully.
+) else (
+    echo punkt already exists.
 )
 
 echo Setup completed successfully.
